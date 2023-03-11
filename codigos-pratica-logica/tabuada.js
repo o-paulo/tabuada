12c882b79e454108aa92a662bad9a98c27bd26a6
Paulo Pereira | PROA.AI <paulo.pereira@proa.ai>
2023-03-11 14:54:42 -0300

    tabuada

---
+++ codigos-pratica-logica/tabuada.js
@@ -0,0 +1,10 @@
+let prmpt = prompt('Digite um numero ')
+
+let n = prmpt
+
+for(x = 1; x <= 10; x++){
+    var calc = n * x
+    console.log(`${n} * ${x}: ${calc}`)
+}
+
+console.log(`O calculo final e ${calc}`)
